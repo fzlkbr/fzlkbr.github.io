884fa8f04fa4009a2fa78a6cc8a60623fe4cbcdb
Faizal Akbar <faizalakbar@live.com>
2016-08-12 22:29:52 +0700

    upload new project

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+/* navbar shrink */
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
+
+/* end navbar shrink */
+
+
+/* smooth scroll */
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+/* end smooth scroll */
